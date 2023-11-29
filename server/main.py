911749fdd3ebeb85b1fd1b13b736602d9ae37e7d
Dallan Quass <dallan@quass.org>
2023-11-29 05:39:24 -0700

    reduce prompt limit

--- server/main.py
+++ server/main.py
@@ -47,7 +47,7 @@ app = FastAPI(debug=debug)
 # init openai
 openai.api_key = openai_key
 # embedding_model = "text-embedding-ada-002"
-prompt_limit = 10000
+prompt_limit = 7000
 
 # init voyageai
 voyageai.api_key = voyageai_key
